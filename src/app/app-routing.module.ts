156d5a435c96f4580a1c5701295a200c99a8fe0f
Shalom Peles <shalom.p@roadmetric.com>
2018-02-20 17:04:37 +0200

    add setting-component & open it in new window

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SettingsComponent } from './components/settings/settings.component';
 
 const routes: Routes = [
     {
         path: '',
+        pathMatch: 'full',
         component: HomeComponent
+    },
+    {
+        path: 'setting',
+        component: SettingsComponent
     }
 ];
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { remote } from 'electron';
+const path = require('path');
+const url = require('url');
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goSetting() {
+    const win = new remote.BrowserWindow({
+      width: 800,
+      height: 600,
+      center: true,
+      resizable: false,
+      frame: true,
+      transparent: false
+    });
+
+    win.loadURL( url.format({
+      pathname: path.join(__dirname, './index.html'),
+      protocol: 'file:',
+      slashes: true,
+      hash: 'setting'
+    }));
+
   }
 
 }
